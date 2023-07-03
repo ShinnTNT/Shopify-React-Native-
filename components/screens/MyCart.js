6eccefb4f68054a7d0d0d8553da091d8d4f48ce8
shinnthant2004 <shinthantequi@gmail.com>
2023-07-03 15:41:48 +0630

    remove item from cart

--- components/screens/MyCart.js
+++ components/screens/MyCart.js
@@ -1,10 +1,259 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { COLOURS, Items } from "../database/Database";
+import { MaterialCommunityIcons } from "react-native-vector-icons";
 
-const MyCart = () => {
+const MyCart = ({ navigation }) => {
+  const [productList, setProductList] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const getDataFromStorage = async () => {
+    let productItems = await AsyncStorage.getItem("cartItems");
+    productItems = JSON.parse(productItems);
+
+    if (productItems) {
+      const cartItems = Items.filter((item) => {
+        return productItems.includes(item.id);
+      });
+      setProductList(cartItems);
+      const totalPrice = cartItems.reduce(
+        (acc, item) => acc + item.productPrice,
+        0
+      );
+      setTotalPrice(totalPrice);
+    }
+  };
+
+  const removeItemFromCart = async (id) => {
+    let itemArray = await AsyncStorage.getItem("cartItems");
+    itemArray = JSON.parse(itemArray);
+    if (itemArray) {
+      let array = itemArray;
+      for (let index = 0; index < array.length; index++) {
+        if (array[index] == id) {
+          array.splice(index, 1);
+        }
+
+        await AsyncStorage.setItem("cartItems", JSON.stringify(array));
+        getDataFromStorage();
+      }
+    }
+  };
+
+  const renderProducts = (data, index) => {
+    return (
+      <TouchableOpacity
+        key={index}
+        style={{
+          width: "100%",
+          height: 100,
+          marginVertical: 6,
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            width: "30%",
+            height: 100,
+            padding: 14,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: COLOURS.backgroundLight,
+            borderRadius: 10,
+            marginRight: 22,
+          }}
+        >
+          <Image
+            source={data.productImage}
+            style={{
+              width: "100%",
+              height: "100%",
+              resizeMode: "contain",
+            }}
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            height: "100%",
+            justifyContent: "space-around",
+          }}
+        >
+          <View>
+            <Text
+              style={{
+                fontSize: 14,
+                maxWidth: "100%",
+                color: COLOURS.black,
+                fontWeight: "600",
+                letterSpacing: 1,
+              }}
+            >
+              {data.productName}
+            </Text>
+            <View
+              style={{
+                marginTop: 4,
+                flexDirection: "row",
+                opacity: 0.6,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 14,
+                  fontWeight: "400",
+                  maxWidth: "85%",
+                  marginRight: 4,
+                }}
+              >
+                &#8377;{data.productPrice}
+              </Text>
+              <Text>
+                (~&#8377; {data.productPrice + data.productPrice / 20})
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  borderRadius: 100,
+                  marginRight: 20,
+                  padding: 4,
+                  borderWidth: 1,
+                  borderColor: COLOURS.backgroundMedium,
+                  opacity: 0.5,
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="minus"
+                  style={{
+                    fontSize: 16,
+                    color: COLOURS.backgroundDark,
+                  }}
+                />
+              </View>
+              <Text>1</Text>
+              <View
+                style={{
+                  borderRadius: 100,
+                  marginLeft: 20,
+                  padding: 4,
+                  borderWidth: 1,
+                  borderColor: COLOURS.backgroundMedium,
+                  opacity: 0.5,
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="plus"
+                  style={{
+                    fontSize: 16,
+                    color: COLOURS.backgroundDark,
+                  }}
+                />
+              </View>
+            </View>
+
+            <TouchableOpacity onPress={() => removeItemFromCart(data.id)}>
+              <MaterialCommunityIcons
+                name="delete"
+                style={{
+                  fontSize: 16,
+                  color: COLOURS.backgroundDark,
+                  backgroundColor: COLOURS.backgroundLight,
+                  padding: 8,
+                  borderRadius: 100,
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      getDataFromStorage();
+    });
+    return unsubscribe;
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>MyCart</Text>
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: COLOURS.white,
+      }}
+    >
+      <ScrollView>
+        <View
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingTop: 16,
+            paddingHorizontal: 16,
+          }}
+        >
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <MaterialCommunityIcons
+              name="chevron-left"
+              style={{
+                fontSize: 18,
+                color: COLOURS.backgroundDark,
+                padding: 12,
+                backgroundColor: COLOURS.backgroundLight,
+                borderRadius: 12,
+              }}
+            />
+          </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: 14,
+              color: COLOURS.black,
+              fontWeight: "400",
+            }}
+          >
+            Order Details
+          </Text>
+        </View>
+        <Text
+          style={{
+            fontSize: 20,
+            color: COLOURS.black,
+            fontWeight: "500",
+            letterSpacing: 1,
+            paddingTop: 20,
+            paddingLeft: 16,
+            marginBottom: 10,
+          }}
+        >
+          My Cart
+        </Text>
+        <View
+          style={{
+            paddingHorizontal: 16,
+          }}
+        >
+          {productList && productList.map(renderProducts)}
+        </View>
+      </ScrollView>
     </View>
   );
 };
